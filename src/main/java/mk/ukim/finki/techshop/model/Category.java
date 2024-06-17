466332cb939f52abe730209ab883fc4c5d1b7f84
Vedran <vedrandeskovski5@gmail.com>
2024-06-17 17:00:53 +0200

    Using postgres DN and adding a new category

--- src/main/java/mk/ukim/finki/techshop/model/Category.java
+++ src/main/java/mk/ukim/finki/techshop/model/Category.java
@@ -2,6 +2,6 @@ package mk.ukim.finki.techshop.model;
 
 public enum Category {
     GPU, CPU, RAM, HDD, SSD, PSU, MOTHERBOARD,
-    KEYBOARD, MOUSE, MONITOR, HEADSET,
+    KEYBOARD, MOUSE, MONITOR, HEADSET, CAMERA,
     CASE, FANS, COOLER, CABLES
 }
